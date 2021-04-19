23014b5e1cf431327159a3b64c382ff3768cc708
Jigar <Jigarpachchigar5561@gmail.com>
2021-04-19 19:21:26 -0400

    Added interface toolbartitle

---
+++ app/src/main/java/capstone/techmatrix/beacondetector/interfaces/ToolbarTitle.java
@@ -0,0 +1,7 @@
+package capstone.techmatrix.beacondetector.interfaces;
+
+
+public interface ToolbarTitle {
+    void setToolbarTitle(String toolbarTitle);
+    void saveSubcategoryTitle(String toolbaTitle);
+}
