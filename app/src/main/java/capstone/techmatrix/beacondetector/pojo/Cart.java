7f307d3ba8c304c1aefe4925ee79e86072c54b66
Jigar <Jigarpachchigar5561@gmail.com>
2021-04-20 03:32:23 -0400

    cart pojo class added

---
+++ app/src/main/java/capstone/techmatrix/beacondetector/pojo/Cart.java
@@ -0,0 +1,42 @@
+package capstone.techmatrix.beacondetector.pojo;
+
+
+public class Cart {
+
+    private Integer id;
+    private Integer itemQuantity;
+    private Product product;
+    private Variant variant;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public Variant getVariant() {
+        return variant;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setVariant(Variant variant) {
+        this.variant = variant;
+    }
+
+    public Integer getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(Integer itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+}
