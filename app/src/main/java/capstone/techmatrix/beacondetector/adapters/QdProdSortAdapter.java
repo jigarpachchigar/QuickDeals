e42cc59fe4ca2dfe08fe342a851c6bd454854361
Jigar <Jigarpachchigar5561@gmail.com>
2021-04-20 12:02:54 -0400

    minor changes done in style and theme

---
+++ app/src/main/java/capstone/techmatrix/beacondetector/adapters/QdProdSortAdapter.java
@@ -0,0 +1,68 @@
+package capstone.techmatrix.beacondetector.adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import capstone.techmatrix.beacondetector.R;
+
+public class QdProdSortAdapter extends BaseAdapter {
+
+    private LayoutInflater inflater;
+    private String[] sortBy;
+    private int selectedId;
+
+    public QdProdSortAdapter(Context context, String[] sortBy, int selectedId) {
+        this.sortBy = sortBy;
+        this.selectedId = selectedId;
+        inflater = (LayoutInflater) context.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return sortBy.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return sortBy[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @SuppressLint({"ViewHolder", "InflateParams"})
+    @Override
+    public View getView(final int position, View view, ViewGroup viewGroup) {
+        // TODO Auto-generated method stub
+        Holder holder = new Holder();
+        View rowView;
+
+        rowView = inflater.inflate(R.layout.qdeals_sort_prod_list, null);
+        holder.name = rowView.findViewById(R.id.name);
+        holder.tick = rowView.findViewById(R.id.tick);
+
+        holder.name.setText(sortBy[position]);
+
+        if (position == selectedId) {
+            holder.tick.setVisibility(View.VISIBLE);
+        } else {
+            holder.tick.setVisibility(View.GONE);
+        }
+
+        return rowView;
+    }
+
+    public class Holder {
+        TextView name;
+        ImageView tick;
+    }
+}
