7c94ece5f23e8d63f63c491f60265ae957db5165
Jigar <Jigarpachchigar5561@gmail.com>
2021-04-20 03:13:45 -0400

    clearing preference in session manager

---
+++ app/src/main/java/capstone/techmatrix/beacondetector/database/SessionManager.java
@@ -0,0 +1,32 @@
+package capstone.techmatrix.beacondetector.database;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SessionManager {
+
+    private SharedPreferences SharedPref;
+    private SharedPreferences.Editor SharedPref_Editor;
+
+    @SuppressLint("CommitPrefEdits")
+    public SessionManager(Context context) {
+        this.SharedPref = context.getSharedPreferences("capstone.techmatrix.beacondetector.database.PREFERENCE.", Activity.MODE_PRIVATE);
+        this.SharedPref_Editor = SharedPref.edit();
+    }
+
+    public void saveSession(String Key, String Value) {
+        SharedPref_Editor.putString(Key, Value);
+        SharedPref_Editor.commit();
+    }
+
+    public String getSessionData(String Key) {
+        return SharedPref.getString(Key, "");
+    }
+
+    public void clearPreferences() {
+        SharedPref_Editor.clear();
+        SharedPref_Editor.commit();
+    }
+}
