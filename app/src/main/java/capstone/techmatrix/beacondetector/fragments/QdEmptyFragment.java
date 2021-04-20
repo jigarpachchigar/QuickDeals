64377f373739a150e967b0e0f7306aaf7bffa676
Jigar <Jigarpachchigar5561@gmail.com>
2021-04-19 23:18:05 -0400

    Added empty fragment for removing fragment with animation

---
+++ app/src/main/java/capstone/techmatrix/beacondetector/fragments/QdEmptyFragment.java
@@ -0,0 +1,31 @@
+package capstone.techmatrix.beacondetector.fragments;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import capstone.techmatrix.beacondetector.R;
+
+
+public class QdEmptyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.blank, container, false);
+    }
+
+
+    protected void onAnimationRepeated () {}
+}
