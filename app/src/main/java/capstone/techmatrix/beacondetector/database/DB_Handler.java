a27974912560f3fe02f0968cc4b23d536872aa51
Jigar <Jigarpachchigar5561@gmail.com>
2021-04-20 03:09:08 -0400

    tables and database created

--- app/src/main/java/capstone/techmatrix/beacondetector/database/DB_Handler.java
+++ app/src/main/java/capstone/techmatrix/beacondetector/database/DB_Handler.java
@@ -58,5 +58,99 @@ public class DB_Handler extends SQLiteOpenHelper {
     private static final String QDUserOrder = "order_history";
     private static final String QDUserCart = "shopping_cart";
 
+    // Create User Table
+    private static final String CREATE_USER_TABLE = "CREATE TABLE " + QDUsers + "("
+            + EMAIL + " TEXT PRIMARY KEY,"
+            + NAME + " TEXT NOT NULL,"
+            + MOBILE + " TEXT NOT NULL,"
+            + PASSWORD + " TEXT NOT NULL" + ")";
+
+    // Create QdProdCategory Table
+    private static final String CREATE_CATEGORIES_TABLE = "CREATE TABLE " + QDCategories + "("
+            + ID + " INTEGER PRIMARY KEY,"
+            + NAME + " TEXT NOT NULL" + ")";
+
+    // Create QdProdSubCategory Mapping Table
+    private static final String CREATE_SUBCATEGORIES_MAPPING_TABLE = "CREATE TABLE " + QDSubCategories + "("
+            + ID + " INTEGER PRIMARY KEY,"
+            + CAT_ID + " INTEGER NOT NULL,"
+            + SUB_ID + " INTEGER NOT NULL" + ")";
+
+    // Create QdProducts Table
+    private static final String CREATE_PRODUCTS_TABLE = "CREATE TABLE " + QDProdcuts + "("
+            + ID + " INTEGER PRIMARY KEY,"
+            + CAT_ID + " INTEGER NOT NULL,"
+            + NAME + " TEXT NOT NULL,"
+            + DATE + " TEXT NOT NULL,"
+            + TAX_NAME + " TEXT NOT NULL,"
+            + TAX_VALUE + " REAL NOT NULL,"
+            + VIEW_COUNT + " INTEGER NOT NULL,"
+            + ORDER_COUNT + " INTEGER NOT NULL,"
+            + SHARE_COUNT + " INTEGER NOT NULL" + ")";
+
+    // Create Variants Table
+    private static final String CREATE_VARIANTS_TABLE = "CREATE TABLE " + QDVar + "("
+            + ID + " INTEGER PRIMARY KEY,"
+            + SIZE + " TEXT,"
+            + COLOR + " TEXT NOT NULL,"
+            + PRICE + " TEXT NOT NULL,"
+            + PDT_ID + " INTEGER NOT NULL" + ")";
+
+    // Create Order History Table
+    private static final String CREATE_ORDER_HISTORY_TABLE = "CREATE TABLE " + QDUserOrder + "("
+            + ID + " INTEGER PRIMARY KEY,"
+            + PDT_ID + " INTEGER NOT NULL,"
+            + VAR_ID + " INTEGER NOT NULL,"
+            + QUANTITY + " INTEGER NOT NULL,"
+            + EMAIL + " TEXT NOT NULL" + ")";
+
+    // Create Shopping Cart Table
+    private static final String CREATE_SHOPPING_CART_TABLE = "CREATE TABLE " + QDUserCart + "("
+            + ID + " INTEGER PRIMARY KEY,"
+            + PDT_ID + " INTEGER NOT NULL,"
+            + VAR_ID + " INTEGER NOT NULL,"
+            + QUANTITY + " INTEGER NOT NULL,"
+            + EMAIL + " TEXT NOT NULL" + ")";
+
+    // Create Wish List Table
+    private static final String CREATE_WISHLIST_TABLE = "CREATE TABLE " + QDWishlist + "("
+            + ID + " INTEGER PRIMARY KEY,"
+            + PDT_ID + " INTEGER NOT NULL,"
+            + EMAIL + " TEXT NOT NULL" + ")";
+
+    public DB_Handler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Creating tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_USER_TABLE);
+        db.execSQL(CREATE_CATEGORIES_TABLE);
+        db.execSQL(CREATE_PRODUCTS_TABLE);
+        db.execSQL(CREATE_VARIANTS_TABLE);
+        db.execSQL(CREATE_SUBCATEGORIES_MAPPING_TABLE);
+        db.execSQL(CREATE_ORDER_HISTORY_TABLE);
+        db.execSQL(CREATE_SHOPPING_CART_TABLE);
+        db.execSQL(CREATE_WISHLIST_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + QDUsers);
+        db.execSQL("DROP TABLE IF EXISTS " + QDCategories);
+        db.execSQL("DROP TABLE IF EXISTS " + QDProdcuts);
+        db.execSQL("DROP TABLE IF EXISTS " + QDVar);
+        db.execSQL("DROP TABLE IF EXISTS " + QDSubCategories);
+        db.execSQL("DROP TABLE IF EXISTS " + QDSubCategories);
+        db.execSQL("DROP TABLE IF EXISTS " + QDUserCart);
+        db.execSQL("DROP TABLE IF EXISTS " + QDWishlist);
+
+        // Create tables
+        onCreate(db);
+    }
+
 
 }
