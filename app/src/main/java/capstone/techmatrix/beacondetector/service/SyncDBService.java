a012653fa1f30ba42e04dd81fed5213afa8d3dc2
Jigar <Jigarpachchigar5561@gmail.com>
2021-04-20 04:07:15 -0400

    get categories of product and save in db

--- app/src/main/java/capstone/techmatrix/beacondetector/service/SyncDBService.java
+++ app/src/main/java/capstone/techmatrix/beacondetector/service/SyncDBService.java
@@ -80,7 +80,108 @@ public class SyncDBService extends IntentService {
         });
     }
 
+    // Process JSON and Save In Local DB
+    private void processData(ResponseJSON responseJSON) {
+        try {
 
+            // Get Categories
+            List<Category> categoryList = responseJSON.getCategories();
+            for (int i = 0; i < categoryList.size(); i++) {
+
+                int CategoryID = responseJSON.getCategories().get(i).getId();
+                String CategoryName = responseJSON.getCategories().get(i).getName();
+                System.out.println("----------------------------");
+                System.out.println("CategoryId : " + CategoryID);
+                System.out.println("CategoryName : " + CategoryName);
+                System.out.println("----------------------------");
+
+                if ((CategoryID == 11 && CategoryName.equalsIgnoreCase("Electronics")) || (CategoryID == 12 && CategoryName.equalsIgnoreCase("Mobiles")) || (CategoryID == 13 && CategoryName.equalsIgnoreCase("Laptops"))
+                        || (CategoryID == 14 && CategoryName.equalsIgnoreCase("Apple")) || (CategoryID == 15 && CategoryName.equalsIgnoreCase("Samsung")) || (CategoryID == 16 && CategoryName.equalsIgnoreCase("Dell")) || (CategoryID == 17 && CategoryName.equalsIgnoreCase("Toshiba"))) {
+                    // insert category into local DB
+                    db_handler.insertCategories(CategoryID, CategoryName);
+
+                    // Get Products
+                    List<Product> productList = responseJSON.getCategories().get(i).getProducts();
+                    for (int j = 0; j < productList.size(); j++) {
+                        int ProductID = productList.get(j).getId();
+                        String ProductName = productList.get(j).getName();
+                        String Date = productList.get(j).getDateAdded();
+                        String TaxName = productList.get(j).getTax().getName();
+                        Double TaxValue = productList.get(j).getTax().getValue();
+
+                        // insert products into local DB
+                        db_handler.insertProducts(ProductID, CategoryID, ProductName, Date, TaxName, TaxValue);
+
+                        // Get Variants
+                        List<Variant> variantList = productList.get(j).getVariants();
+                        for (int p = 0; p < variantList.size(); p++) {
+                            int VariantID = variantList.get(p).getId();
+                            String Size = null;
+                            String Color = variantList.get(p).getColor();
+                            String Price = String.valueOf(variantList.get(p).getPrice());
+
+                            try {
+                                // Size May Produce NullPointerException
+                                Size = variantList.get(p).getSize().toString();
+                            } catch (NullPointerException ignore) {
+                            }
+
+                            // insert variants into local DB
+                            db_handler.insertVariants(VariantID, Size, Color, Price, ProductID);
+                        }
+                    }
+
+                    // Get Child Categories
+                    List<Integer> childCategories = categoryList.get(i).getChildCategories();
+                    for (int k = 0; k < childCategories.size(); k++) {
+                        int SubcategoryID = childCategories.get(k);
+
+                        // insert childs into subcategory mapping
+                        db_handler.insertChildCategoryMapping(CategoryID, SubcategoryID);
+                    }
+                }
+            }
+            // Get Rankings
+            List<Ranking> rankingList = responseJSON.getRankings();
+            for (int i = 0; i < rankingList.size(); i++) {
+                // Get Products Rank List
+                List<ProductRank> productRankList = rankingList.get(i).getProducts();
+                for (int j = 0; j < productRankList.size(); j++) {
+                    try {
+                        int id = productRankList.get(j).getId();
+                        switch (i) {
+                            case 0: // Most Viewed Products
+                                int viewCount = productRankList.get(j).getViewCount();
+
+                                // update product table
+                                db_handler.updateCounts(DB_Handler.VIEW_COUNT, viewCount, id);
+                                break;
+
+                            case 1: // Most Ordered Products
+                                int orderCount = productRankList.get(j).getOrderCount();
+
+                                // update product table
+                                db_handler.updateCounts(DB_Handler.ORDER_COUNT, orderCount, id);
+                                break;
+
+                            case 2: // Most Shared Products
+                                int shareCount = productRankList.get(j).getShares();
+
+                                // update product table
+                                db_handler.updateCounts(DB_Handler.SHARE_COUNT, shareCount, id);
+                                break;
+                        }
+                    } catch (Exception ignore) {
+                    }
+                }
+            }
+
+            reply("success");
+            Log.i("DB Sync","success");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     // Reply Message
     private void reply(String value)
